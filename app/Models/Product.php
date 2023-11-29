55c7df88c76bb91bd17427fce7122cac2cbf71eb
Razoana Ayshee <razoanaayshee1@gmail.com>
2023-11-30 00:16:58 +0600

    Product controller v3

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @method static select(string[] $array)
@@ -13,4 +14,9 @@ class Product extends Model
 {
     use HasFactory;
     protected $guarded=['id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
